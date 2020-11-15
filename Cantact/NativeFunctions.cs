f66782467e36ea9bc322cf438e757864706cafa5
Elliot Woods <elliot.woods@gmail.com>
2020-11-15 23:08:26 +0900

    first commit

---
+++ Cantact/NativeFunctions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Cantact
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct CantactFrame
+	{
+		public byte channel;
+		public UInt32 id;
+
+		public byte dlc;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+		public byte[] data;
+
+		public byte ext;
+		public byte fd;
+		public byte loopback;
+		public byte rtr;
+	}
+
+	public class NativeFunctions
+	{
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static IntPtr cantact_init();
+
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_deinit(IntPtr handle);
+
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_open(IntPtr handle);
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_close(IntPtr handle);
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_start(IntPtr handle);
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_stop(IntPtr handle);
+
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_transmit(IntPtr handle, CantactFrame cantactFrame);
+
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_set_bitrate(IntPtr handle, byte channel, UInt32 bitrate);
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_set_enabled(IntPtr handle, byte channel, byte enabled);
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_set_monitor(IntPtr handle, byte channel, byte enabled);
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_set_hw_loopback(IntPtr handle, byte channel, byte enabled);
+
+
+		[DllImport("cantact.dll", CallingConvention = CallingConvention.StdCall)]
+		public extern static Int32 cantact_get_channel_count(IntPtr handle);
+	}
+}
---
+++ CantactTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Cantact
+{
+	class Program
+	{
+		static unsafe void Main(string[] args)
+		{
+			Console.WriteLine("Initializing driver");
+			var handle = NativeFunctions.cantact_init();
+
+
+			Console.WriteLine("Opening ");
+			if(NativeFunctions.cantact_open(handle) != 0)
+			{
+				throw (new Exception("Failed to open driver"));
+			}
+
+			Console.WriteLine("Getting channel count:");
+			var channelCount = NativeFunctions.cantact_get_channel_count(handle);
+			Console.WriteLine(channelCount);
+
+			Console.WriteLine("Closing (just following how it works in BusMaster)");
+			NativeFunctions.cantact_close(handle);
+
+			CantactFrame frame = new CantactFrame();
+			{
+				frame.channel = 0;
+				frame.id = 1 << 19;
+
+				{
+					frame.data = new byte[8];
+					var binaryWriter = new BinaryWriter(new MemoryStream(frame.data));
+					binaryWriter.Write((byte)1);
+					binaryWriter.Write((UInt16)1);
+					binaryWriter.Write((Int32)1);
+				}
+			
+				frame.dlc = 7;
+				frame.ext = 1;
+				frame.fd = 0;
+				frame.rtr = 0;
+			}
+
+			Console.WriteLine("Setting bit rate");
+			NativeFunctions.cantact_open(handle);
+			NativeFunctions.cantact_set_enabled(handle, 0, 1);
+			if(NativeFunctions.cantact_set_bitrate(handle, 0, 500000) < 0)
+			{
+				throw (new Exception("Failed to set bitrate"));
+			}
+			NativeFunctions.cantact_close(handle);
+
+			Console.WriteLine("Starting");
+
+			NativeFunctions.cantact_open(handle);
+			if (NativeFunctions.cantact_start(handle) != 0) {
+				throw (new Exception("Failed to start device"));
+			}
+
+			Console.WriteLine("Transmitting");
+			Marshal.SizeOf(typeof(CantactFrame));
+			//var result = NativeFunctions.cantact_transmit(handle, frame);
+			//Console.WriteLine(result);
+
+			NativeFunctions.cantact_stop(handle);
+			NativeFunctions.cantact_close(handle);
+			NativeFunctions.cantact_deinit(handle);
+		}
+	}
+}
